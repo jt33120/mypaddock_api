d56dfd6555583f255d67ca509fa6008ad515c87b
Julian Talou <julian.talou@polytechnique.edu>
2025-12-02 14:59:04 +0100

    Add CORS middleware configuration

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://mypaddock.vercel.app",  # <-- add your real frontend domain
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
